e5e11c282a7ccc22ae4df75d6f993ac55da529df
ZedLin <zed.lin@foxmail.com>
2020-11-06 14:08:08 +0800

    URL patterns for changing password views

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -8,4 +8,11 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
+    # change password urls
+    path('password_change/',   #  http://127.0.0.1:8000/account/password_change/
+         auth_views.PasswordChangeView.as_view(),
+         name='password_change'),
+    path('password_change/done/', 
+         auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
 ]
