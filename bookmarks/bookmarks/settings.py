82eb4f08313d828f61e7e6702f9fac63e24ede28
ZedLin <zed.lin@foxmail.com>
2020-11-06 10:59:00 +0800

    setting.py : hardcoded URLs instead of URL names

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# configured your settings for Django to use these URLs by default
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
