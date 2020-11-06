e028765b9c80c5f5b55f7c08cc5f699bf1f89c5b
ZedLin <zed.lin@foxmail.com>
2020-11-06 17:58:50 +0800

    用 Django 提供的 authentication URL patterns 替代先前自建的

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -8,6 +8,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
+    
     # change password urls
     path('password_change/',   #  http://127.0.0.1:8000/account/password_change/
          auth_views.PasswordChangeView.as_view(),
@@ -15,21 +16,23 @@ urlpatterns = [
     path('password_change/done/', 
          auth_views.PasswordChangeDoneView.as_view(),
          name='password_change_done'),
+    
     # reset password urls
     # 邮箱表单页
-    path('password_reset/',
-         auth_views.PasswordResetView.as_view(),
-         name='password_reset'),
-    # 邮件发送完成页
-    path('password_reset/done/',
-         auth_views.PasswordResetDoneView.as_view(),
-         name='password_reset_done'),
-    # 密码重置表单页
-    path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(),
-         name='password_reset_confirm'),
-    # 完成密码重置
-    path('reset/done/',
-         auth_views.PasswordResetCompleteView.as_view(),
-         name='password_reset_complete'),
+    # path('password_reset/',
+    #      auth_views.PasswordResetView.as_view(),
+    #      name='password_reset'),
+    # # 邮件发送完成页
+    # path('password_reset/done/',
+    #      auth_views.PasswordResetDoneView.as_view(),
+    #      name='password_reset_done'),
+    # # 密码重置表单页
+    # path('reset/<uidb64>/<token>/',
+    #      auth_views.PasswordResetConfirmView.as_view(),
+    #      name='password_reset_confirm'),
+    # # 完成密码重置
+    # path('reset/done/',
+    #      auth_views.PasswordResetCompleteView.as_view(),
+    #      name='password_reset_complete'),
+    path('', include('django.contrib.auth.urls')),
 ]
