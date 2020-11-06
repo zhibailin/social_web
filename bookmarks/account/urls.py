afe5e4f7b8f3b4438d0fd73f91e2948f005d11cd
ZedLin <zed.lin@foxmail.com>
2020-11-06 15:42:42 +0800

    URL pattern and template for `PasswordReset Confirm View`

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -24,4 +24,8 @@ urlpatterns = [
     path('password_reset/done/',
          auth_views.PasswordResetDoneView.as_view(),
          name='password_reset_done'),
+    # 密码重置表单页
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
 ]
