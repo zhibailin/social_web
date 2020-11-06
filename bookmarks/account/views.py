8a1adf4ce2450a7eece342efa36146f34e181b35
ZedLin <zed.lin@foxmail.com>
2020-11-06 10:45:15 +0800

    view : create a new view to display a dashboard when users log in to their account

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
-
+from django.contrib.auth.decorators import login_required
 
 def user_login(request):
     if request.method == 'POST':               # 当通过 POST request 调用 user_login 时
@@ -23,3 +23,14 @@ def user_login(request):
     else:
         form = LoginForm()                      # 当通过 GET request 调用 user_login 时，创建一个 LoginForm 实例传入 template
     return render(request, 'account/login.html', {'form': form})
+
+# The login_required decorator checks whether the current user is authenticated. 
+# If the user is authenticated, it executes the decorated view; 
+# if the user is not authenticated, it redirects the user to the login URL 
+# with the originally requested URL as a GET parameter named `next` (
+# http://127.0.0.1:8000/account/login/?next=/account/).
+@login_required
+def dashboard(request):
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
