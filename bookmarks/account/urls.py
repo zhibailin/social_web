d68143ecbbc626ab7ec0351820bc385bc9aa39d8
ZedLin <zed.lin@foxmail.com>
2020-11-07 13:39:38 +0800

    URL pattern for views.register

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -35,4 +35,5 @@ urlpatterns = [
     #      auth_views.PasswordResetCompleteView.as_view(),
     #      name='password_reset_complete'),
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
 ]
