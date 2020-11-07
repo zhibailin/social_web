754777333129fbb556261029a2bc23196ee0f684
ZedLin <zed.lin@foxmail.com>
2020-11-07 12:24:49 +0800

    ModelForm : User registration

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,6 +1,27 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label="Password",
+                               widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password',
+                                widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+    
+    # a `clean_<fieldname>()`` method to any of your form fields 
+    # in order to clean the value or raise form validation errors 
+    # for a specific field.
+    # This check is done when you validate the form by calling `is_valid()`
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
