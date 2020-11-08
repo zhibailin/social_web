e272783cf839dce037d184f074f44b6632a82245
ZedLin <zed.lin@foxmail.com>
2020-11-08 13:27:06 +0800

    create a custom authentication backend

---
+++ bookmarks/account/authentication.py
@@ -0,0 +1,24 @@
+# create an authentication backend 
+# to let users authenticate in your site using their email address 
+# instead of their username.
+
+from django.contrib.auth.models import User
+
+
+# An authentication backend is a class that provides 
+# `authenticate()` and `get_user()` methods
+class EmailAuthBackend(object):
+    def authenticate(self, request, username=None, password=None):
+        try:
+            user = User.objects.get(email=username)
+            if user.check_password(password):
+                return user
+            return None
+        except User.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
