ede4209a168e7fccf5170c741004c47a3ad7157a
ZedLin <zed.lin@foxmail.com>
2020-11-08 09:10:52 +0800

    注释：where is the `messages` variable  come from

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -58,11 +58,11 @@ TEMPLATES = [
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
-            'context_processors': [
+            'context_processors': [                                     # context_processors list
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                'django.contrib.messages.context_processors.messages',  # adds a messages variable to the request context
             ],
         },
     },
