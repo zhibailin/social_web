5686dd540942d8d0f40ed007eb3a4613209acf35
ZedLin <zed.lin@foxmail.com>
2020-11-09 08:50:16 +0800

    define a `Image` model to store images and their information

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Image(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             related_name='images_created',
+                             on_delete=models.CASCADE)        # related images are also deleted when a user is deleted
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200,                   # for building beautiful SEO-friendly URLs
+                            blank=True)
+    url = models.URLField()
+    image = models.ImageField(upload_to='images/%Y/%m/%d/')
+    description = models.TextField(blank=True)
+    created = models.DateField(auto_now_add=True,
+                               db_index=True)                 # creates an index in the database for this field
+    
+    def __str__(self):
+        return self.title
