7a734b1eb8f814b43f9320b2040c2833a9cec3b4
ZedLin <zed.lin@foxmail.com>
2020-11-09 10:30:27 +0800

    form : override the `save()` method to download the image file and save it

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Image
+from urllib import request
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 
 
 class ImageCreateForm(forms.ModelForm):
@@ -21,3 +24,22 @@ class ImageCreateForm(forms.ModelForm):
         if extension not in valid_extensions:
             raise forms.ValidationError('The given URL does not match valid image extensions.')
         return url
+    
+    # to download the image file and save it
+    def save(self, force_insert=False,
+                   force_update=False,
+                   commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+        
+        # download image from the given URL
+        response = request.urlopen(image_url)
+        image.image.save(image_name,
+                         ContentFile(response.read()),
+                         save=False)
+        if commit:
+            image.save()
+        return image
