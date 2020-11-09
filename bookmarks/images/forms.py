4ac04d559bb598ed1b840d6a0fb69972c9866f96
ZedLin <zed.lin@foxmail.com>
2020-11-09 10:11:20 +0800

    form : define a `clean_url()` method to clean the `url` field

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -12,3 +12,12 @@ class ImageCreateForm(forms.ModelForm):
         widgets = {
             'url': forms.HiddenInput,           
         }
+
+    # to verify that the provided image URL is valid, only allow JPEG files
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpg', 'jpeg']
+        extension = url.rsplit('.', 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('The given URL does not match valid image extensions.')
+        return url
