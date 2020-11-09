2b83891ad6685c804b845fb98db8d49fe0dc99a4
ZedLin <zed.lin@foxmail.com>
2020-11-09 12:35:22 +0800

    view : 添加注释

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -9,16 +9,21 @@ def image_create(request):
         # form is sent
         form = ImageCreateForm(data=request.POST)
         if form.is_valid():
+            # if form data is valid, create a new Image instance, not be saved to the database
             cd = form.cleaned_data
             new_item = form.save(commit=False)
             
+            # who uploaded each image: assign current user to the new `image` object
             new_item.user = request.user
             new_item.save()
             messages.success(request, 'Image added successfully')
             
-            return redirect(new_item.get_absolue_url())
+            # redirect to new created item detail view
+            # TODO: implemented the get_absolute_url() method of the Image model
+            return redirect(new_item.get_absolute_url())
     else:
         # build form with data provided by the bookmarklet via GET by the JavaScript tool
+        # TODO: the JavaScript tool
         form = ImageCreateForm(data=request.GET)
     
     return render(request,
