8862a4ec9c9dbc547a07a4bd999bdc1418dc6e3b
ZedLin <zed.lin@foxmail.com>
2020-11-10 11:03:03 +0800

    find the images on the website

--- bookmarks/images/static/js/bookmarklet.js
+++ bookmarks/images/static/js/bookmarklet.js
@@ -24,6 +24,15 @@
         jQuery('#bookmarklet #close').click(function(){
             jQuery('#bookmarklet').remove();
         });
+
+        // find images and display them
+        jQuery.each(jQuery('img[src$="jpg"]'),function(index, image) {
+            if (jQuery(image).width() >= min_width && jQuery(image).height() >= min_height)
+            {
+                image_url = jQuery(image).attr('src');
+                jQuery('#bookmarklet .images').append('<a href="#"><img src="'+image_url +'" /></a>');
+            }
+        })
     };
 
     // Check if jQuery is loaded
