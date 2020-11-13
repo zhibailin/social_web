111ab67ddd670583d70e79b2d27f2dc11a132914
ZedLin <zed.lin@foxmail.com>
2020-11-13 10:19:34 +0800

    model : set up a generic relation in the `action` model

--- bookmarks/actions/models.py
+++ bookmarks/actions/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 class Action(models.Model):
@@ -7,8 +9,20 @@ class Action(models.Model):
                              db_index=True,
                              on_delete=models.CASCADE)
     verb = models.CharField(max_length=255)
+    # A `ForeignKey` field that points to the `ContentType` model
+    target_ct = models.ForeignKey(ContentType,
+                                  blank=True,
+                                  null=True,
+                                  related_name='target_obj',
+                                  on_delete=models.CASCADE)
+    # A `PositiveIntegerField` for storing the primary key of the related object
+    target_id = models.PositiveIntegerField(null=True,
+                                            blank=True,
+                                            db_index=True)
+    # A GenericForeignKey field to the related object based on the combination of the two previous fields
+    target = GenericForeignKey('target_ct', 'target_id')
     created = models.DateTimeField(auto_now_add=True,
                                    db_index=True)
-    
+
     class Meta:
         ordering = ('-created',)
